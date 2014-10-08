81faf325c6586db96e4f6384394ae405925bdf3b
Mathieu Méa <mathieu.mea@gmail.com>
2014-10-08 10:05:37 -0400

    Improve label cleanup

--- src/org/mtransit/parser/ca_laval_stl_bus/LavalSTLBusAgencyTools.java
+++ src/org/mtransit/parser/ca_laval_stl_bus/LavalSTLBusAgencyTools.java
@@ -11,6 +11,7 @@ import org.mtransit.parser.gtfs.data.GRoute;
 import org.mtransit.parser.gtfs.data.GStop;
 import org.mtransit.parser.gtfs.data.GTrip;
 import org.mtransit.parser.mt.data.MDirectionType;
+import org.mtransit.parser.mt.data.MSpec;
 import org.mtransit.parser.mt.data.MTrip;
 
 // http://www.stl.laval.qc.ca/en/stl-synchro/developers/
@@ -112,11 +113,26 @@ public class LavalSTLBusAgencyTools extends DefaultAgencyTools {
 
 	private static final Pattern REMOVE_STOP_CODE_STOP_NAME = Pattern.compile("\\[[0-9]{5}\\]");
 
+	private static final Pattern START_WITH_FACE_A = Pattern.compile("^(face à )", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+	private static final Pattern START_WITH_FACE_AU = Pattern.compile("^(face au )", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+	private static final Pattern START_WITH_FACE = Pattern.compile("^(face )", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+
+	private static final Pattern SPACE_FACE_A = Pattern.compile("( face à )", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+	private static final Pattern SPACE_WITH_FACE_AU = Pattern.compile("( face au )", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+	private static final Pattern SPACE_WITH_FACE = Pattern.compile("( face )", Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+
+	private static final Pattern[] START_WITH_FACES = new Pattern[] { START_WITH_FACE_A, START_WITH_FACE_AU, START_WITH_FACE };
+
+	private static final Pattern[] SPACE_FACES = new Pattern[] { SPACE_FACE_A, SPACE_WITH_FACE_AU, SPACE_WITH_FACE };
+
 	@Override
 	public String cleanStopName(String gStopName) {
 		String result = gStopName;
-		result = REMOVE_STOP_CODE_STOP_NAME.matcher(result).replaceAll("");
-		return super.cleanStopName(result);
+		result = REMOVE_STOP_CODE_STOP_NAME.matcher(result).replaceAll(StringUtils.EMPTY);
+		result = MSpec.CLEAN_SLASHES.matcher(result).replaceAll(MSpec.CLEAN_SLASHES_REPLACEMENT);
+		result = Utils.replaceAll(result, START_WITH_FACES, MSpec.SPACE);
+		result = Utils.replaceAll(result, SPACE_FACES, MSpec.SPACE);
+		return super.cleanStopNameFR(result);
 	}
 
 }
